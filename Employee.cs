01d01795423fe0f3634e98ff467d8aa0a6ef8291
Jerome <josejeromelalunio@gmail.com>
2022-01-18 15:36:44 +0800

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollProgram
+{
+    class Employee
+    {
+        public string name;
+        public string position;
+        public int salary;
+        public bool isPaid = false;
+
+        Employee(string name, string position, int salary)
+        {
+            this.name = name;
+            this.position = position;
+            this.salary = salary;
+        }
+    }
+}
